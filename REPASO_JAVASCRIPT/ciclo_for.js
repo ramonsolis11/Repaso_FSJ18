9d8adbd591f3b80b781921aa7028487339814717
Ramon Solis Nuñez <ramonsolis11@gmail.com>
2023-06-20 21:00:04 -0600

    Class for

---
+++ REPASO_JAVASCRIPT/ciclo_for.js
@@ -0,0 +1,39 @@
+/* for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+} */
+
+/*for (variable de control; condicion ; modificacion de la variable de control) {
+    codigo a ejecutarse
+} */
+
+//imprimir 50 veces un mensaje de Hola!
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(`El valor de i es: $[i]`);
+// }
+
+
+//TODO: tabla de multiplicar del 12 hasta el #25.
+
+// for (let i = 1; i <= 25; i++) {
+//     let resultado = 12 * i;
+//     console.log(`12 * ${i} = ${resultado}`);
+// }
+
+// descendente
+for (let i = 100; i >= 1; i--) {
+    console.log(`El valor de i es: ${i}`);
+}
+
+// ascendente
+for (let j = 1; j <= 100; j++){
+    console.log(`El resultado es: ${j}`);
+}
+
+
+
+
+
+
+
